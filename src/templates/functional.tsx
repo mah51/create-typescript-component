12cf47eb901ae819ee70e3f5524eeed003c1d17d
mah51 <mah51@kent.ac.uk>
2021-06-23 21:16:55 +0100

    first commit

---
+++ src/templates/functional.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface COMPONENT_NAMEProps {
+
+}
+
+const COMPONENT_NAME = (props: COMPONENT_NAMEProps): JSX.Element => {
+  return <div />;
+};
+
+export default COMPONENT_NAME;
